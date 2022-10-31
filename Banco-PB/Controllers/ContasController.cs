1cf5479c1706170b92bf2e6dd4f0d0221dd736fd
Lucas Gomes <lucas.garaujo@al.infnet.edu.br>
2022-10-31 09:44:11 -0300

    configuração para imagem

--- Banco-PB/Controllers/ContasController.cs
+++ Banco-PB/Controllers/ContasController.cs
@@ -8,6 +8,8 @@ using Microsoft.EntityFrameworkCore;
 using Banco_PB.Data;
 using Banco_PB.Models;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
 
 namespace Banco_PB.Controllers
 {
@@ -58,15 +60,32 @@ namespace Banco_PB.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Nome,Saldo")] Conta conta)
+        public async Task<IActionResult> Create(Conta conta, IFormFile Foto)
         {
             if (ModelState.IsValid)
             {
+                conta.Foto = UploadImage(Foto);
                 _context.Add(conta);
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
             return View(conta);
+        }
+
+        private static string UploadImage(IFormFile imageFile)
+        {
+            string connectionString = "DefaultEndpointsProtocol=https;AccountName=xxxresourcer;AccountKey=19LwhIgh1nJiU6BTH6idmZ4W285jkSS3xkowQ3D1vPOQjXe8kWE6ULNG6vrBFgQt+wzqf1f0mAYG+AStBR1/Zw==;EndpointSuffix=core.windows.net";
+            string containerName = "aulapblp";
+            var reader = imageFile.OpenReadStream();
+            var cloundStorageAccount = CloudStorageAccount.Parse(connectionString);
+            var blobClient = cloundStorageAccount.CreateCloudBlobClient();
+            var container = blobClient.GetContainerReference(containerName);
+            container.CreateIfNotExistsAsync();
+            CloudBlockBlob blob = container.GetBlockBlobReference(imageFile.FileName);
+            Thread.Sleep(1000);
+            blob.UploadFromStreamAsync(reader);
+            return blob.Uri.ToString();
+
         }
 
         // GET: Contas/Edit/5
